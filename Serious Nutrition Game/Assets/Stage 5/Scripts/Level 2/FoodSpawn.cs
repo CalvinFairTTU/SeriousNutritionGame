ab182e937a6741e1d4ee7af5828ad3faacc1b290
Christine Huber <christine.e.huber@gmail.com>
2017-10-19 11:05:51 -0500

    S5L2 Creation
    Background and prefabs have been created. Added FoodSpawn script to
    randomly spawn food at the holes. Added Translator script to move food
    up and out of the holes.

---
+++ Serious Nutrition Game/Assets/Stage 5/Scripts/Level 2/FoodSpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawn : MonoBehaviour {
+    public GameObject[] foods;
+    public Transform[] spawns;
+
+    private float timer;
+    private int foodIndex;
+    private int spawnIndex;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InvokeRepeating("SpawnFood", 3f, 7f);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+
+    void SpawnFood ()
+    {
+        foodIndex = Random.Range(0, 6);
+        spawnIndex = Random.Range(0, 4);
+        Instantiate(foods[foodIndex], spawns[spawnIndex].position, spawns[spawnIndex].rotation);
+    }
+
+    public int getSpawnIndex()
+    {
+        return spawnIndex;
+    }
+}
---
+++ Serious Nutrition Game/Assets/Stage 5/Scripts/Level 2/Translator.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translator : MonoBehaviour {
+    private int spawnIndex;
+    private SpriteRenderer sprite;
+
+    void Start ()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (transform.position.x == -2.0)
+            spawnIndex = 0;
+        if (transform.position.x == 2.05)
+            spawnIndex = 1;
+        if (transform.position.x == -1.5)
+            spawnIndex = 2;
+        if (transform.position.x > 1.5 && transform.position.x < 2.0)
+            spawnIndex = 3;
+        
+        switch (spawnIndex)
+        {
+            case 0:
+                sprite.sortingLayerName = "Foreground 01";
+                if (transform.position.y < -1.5)
+                    transform.Translate(new Vector3(0, 5, 0) * Time.deltaTime);
+                Invoke("DespawnFood", 5f);
+                break;
+            case 1:
+                sprite.sortingLayerName = "Foreground 01";
+                if (transform.position.y < -1.5)
+                    transform.Translate(new Vector3(0, 5, 0) * Time.deltaTime);
+                Invoke("DespawnFood", 5f);
+                break;
+            case 2:
+                sprite.sortingLayerName = "Foreground 02";
+                if (transform.position.y < 0.03)
+                    transform.Translate(new Vector3(0, 5, 0) * Time.deltaTime);
+                Invoke("DespawnFood", 5f);
+                break;
+            case 3:
+                sprite.sortingLayerName = "Foreground 02";
+                if (transform.position.y < 0.03)
+                    transform.Translate(new Vector3(0, 5, 0) * Time.deltaTime);
+                Invoke("DespawnFood", 5f);
+                break;
+        }
+	}
+
+    void DespawnFood ()
+    {
+        if (transform.position.x == -2.0)
+            spawnIndex = 0;
+        if (transform.position.x == 2.05)
+            spawnIndex = 1;
+        if (transform.position.x == -1.5)
+            spawnIndex = 2;
+        if (transform.position.x > 1.5 && transform.position.x < 2.0)
+            spawnIndex = 3;
+
+        switch (spawnIndex)
+        {
+            case 0:
+                if (transform.position.y > -2.908023)
+                    transform.Translate(new Vector3(0, -5, 0) * Time.deltaTime);
+                break;
+            case 1:
+                if (transform.position.y > -2.908023)
+                    transform.Translate(new Vector3(0, -5, 0) * Time.deltaTime);
+                break;
+            case 2:
+                if (transform.position.y > -1.068023)
+                    transform.Translate(new Vector3(0, -5, 0) * Time.deltaTime);
+                break;
+            case 3:
+                if (transform.position.y > -1.068023)
+                    transform.Translate(new Vector3(0, -5, 0) * Time.deltaTime);
+                Invoke("DespawnFood", 5f);
+                break;
+        }
+
+        Destroy(this.gameObject);
+    }
+}
