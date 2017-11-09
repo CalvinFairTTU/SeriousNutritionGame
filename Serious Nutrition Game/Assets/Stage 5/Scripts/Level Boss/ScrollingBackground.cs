53390e7797ba7e3488115c19379979a7e20b1bcb
Jordan Alnaser <jordanalnaser@live.com>
2017-11-08 21:56:36 -0600

    Adding Prefabs

--- Serious Nutrition Game/Assets/Stage 5/Scripts/Level Boss/ScrollingBackground.cs
+++ Serious Nutrition Game/Assets/Stage 5/Scripts/Level Boss/ScrollingBackground.cs
@@ -6,10 +6,19 @@ public class ScrollingBackground : MonoBehaviour {
 
     public float Speed;
 
+	private MeshRenderer backgroundRenderer;
+
     // Update is called once per frame
     void Update()
     {
         Vector2 offeset = new Vector2( -Time.time * Speed, 0);
         GetComponent<Renderer>().material.mainTextureOffset = offeset;
     }
+
+	void Start()
+	{
+		backgroundRenderer = this.GetComponent<MeshRenderer>();
+
+		backgroundRenderer.sortingLayerName = "Background";
+	}
 }
