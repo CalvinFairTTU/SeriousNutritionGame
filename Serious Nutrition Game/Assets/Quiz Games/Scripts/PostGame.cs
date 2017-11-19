98aede5df32d899ff0ebb4676781d0b3c65e3f2a
CalvinFairTTU <Calvin.Fair@ttu.edu>
2017-11-19 11:31:13 -0600

    Post Game Quiz Complete

---
+++ Serious Nutrition Game/Assets/Quiz Games/Scripts/PostGame.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PostGame : MonoBehaviour {
+
+	public GameObject foodItemOne;
+	public GameObject foodItemTwo;
+	public GameObject foodItemThree;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void firstFoodOption () {
+		if (foodItemOne.tag == "Good_Food") {
+			Debug.Log("Correct food!");
+		} else {
+			Debug.Log ("Wrong food!");
+		}
+	}
+
+	public void secondFoodOption () {
+        if (foodItemTwo.tag == "Good_Food") {
+            Debug.Log("Correct food!");
+        } else {
+            Debug.Log("Wrong food!");
+        }
+    }
+
+	public void thirdFoodOption () {
+        if (foodItemThree.tag == "Good_Food"){
+            Debug.Log("Correct food!");
+        } else {
+            Debug.Log("Wrong food!");
+        }
+    }
+}
