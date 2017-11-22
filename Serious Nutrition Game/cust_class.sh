f839ed1ef5450a05d3e9979bec4c9423edd4b529
AJEvbank <austin.eubank@ttu.edu>
2017-11-21 19:15:11 -0600

    Still working on running tests.

--- Serious Nutrition Game/cust_class.sh
+++ Serious Nutrition Game/cust_class.sh
@@ -10,10 +10,9 @@ hello_world \
 -projectPath=$(pwd) \
 -exit \
 -batchmode \
--logFile=$(pwd)/test_log.log \
+-logFile \
 -executeMethod testNamespace.testComLineClass.printParamsMethod
 
 
 
 echo "Testing finished..."
-cat test_log.log
