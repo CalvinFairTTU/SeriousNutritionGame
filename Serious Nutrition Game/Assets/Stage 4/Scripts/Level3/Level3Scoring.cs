63692428f194be36bf88cc4e4065242f02fd0e3e
Shashidhar Sastry <shashidsastry@gmail.com>
2017-11-29 14:03:43 -0600

    Level 3 almost complete
    Level 3 has a progress bar, which moves up and down depending on whether a good or bad food is dropped on the plate. Level 3.1 allows for foods to return to the plate, if they are dropped outside the plate. Need to integrate Levels 3 and 3.1

---
+++ Serious Nutrition Game/Assets/Stage 4/Scripts/Level3/Level3Scoring.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level3Scoring : MonoBehaviour {
+
+	public Slider progressBar;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D food) {
+
+		if (food.gameObject.CompareTag ("GoodFood"))
+		if (progressBar.value < 1f)
+			progressBar.value += 0.5f;
+
+		if (food.gameObject.CompareTag ("BadFood"))
+		if (progressBar.value < 1f && progressBar.value > 0f)
+			progressBar.value -= 0.05f;
+
+
+		if(progressBar.value >= 1f) {
+			Debug.Log ("You won the game!");
+			//gameObject.GetComponent<WinGame> ().Win ();
+			//gameObject.GetComponent<EdgeCollider2D> ().enabled = false;
+		}
+	}
+}
