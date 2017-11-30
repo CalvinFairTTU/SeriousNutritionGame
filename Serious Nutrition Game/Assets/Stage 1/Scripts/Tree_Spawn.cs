350f79eab7423cd5dc0eea2dc99ca844f2ca4ebe
Dhruuv Khurana <dhruuv.khurana@ttu.edu>
2017-11-29 20:55:39 -0600

    S1L3 build

---
+++ Serious Nutrition Game/Assets/Stage 1/Scripts/Tree_Spawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree_Spawn : MonoBehaviour 
+{
+	public GameObject[] foods;
+	GameObject food;
+
+	public Transform[] spawn_location;
+	public float dropSpeed;
+	private float minSpawnTime = 1.5f;
+	private float maxSpawnTime = 3f;
+
+	void Start() 
+	{
+		StartCoroutine (Spawn());
+	}
+
+	IEnumerator Spawn()
+	{
+		while (true) {
+			yield return new WaitForSeconds (Random.Range (minSpawnTime, maxSpawnTime));
+			food = Instantiate (foods[Random.Range (0, foods.Length)], spawn_location[Random.Range (0, spawn_location.Length)].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+		}
+	}
+}
