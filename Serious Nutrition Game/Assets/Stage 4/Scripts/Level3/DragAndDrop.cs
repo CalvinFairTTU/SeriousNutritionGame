42bc95b0749f7f432ed84a7a5494230198c56399
CalvinFairTTU <Calvin.Fair@ttu.edu>
2017-11-29 20:08:43 -0600

    Shashi's Branch committed to bypass conflicts.

---
+++ Serious Nutrition Game/Assets/Stage 4/Scripts/Level3/DragAndDrop.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+
+public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public static GameObject itemBeingDragged;
+	public static bool IsOnPlate = false;
+	Vector3 startPosition;
+	Transform startParent;
+
+	#region IBeginDragHandler implementation
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		itemBeingDragged = gameObject;
+		startPosition = transform.position;
+		startParent = transform.parent;
+		GetComponent<CanvasGroup> ().blocksRaycasts = false;
+	}
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		transform.position = Input.mousePosition;
+	}
+
+	#endregion
+
+	#region IEndDragHandler implementation
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+		itemBeingDragged = null;
+		GetComponent<CanvasGroup> ().blocksRaycasts = true;
+		if (transform.parent == startParent) {
+			transform.position = startPosition;
+		}
+
+	}
+
+	#endregion
+
+}
+
+
+/*
+public class DragAndDrop : MonoBehaviour {
+
+	void OnMouseDrag() {
+		Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+		Vector2 objPos = Camera.main.ScreenToWorldPoint(mousePos);
+		transform.position = objPos;
+	}
+}
+*/
