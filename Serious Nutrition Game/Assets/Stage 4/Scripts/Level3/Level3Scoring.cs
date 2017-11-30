76d123e78bb8b2169b1b90641adfad1cae29323c
Shashidhar Sastry <shashidsastry@gmail.com>
2017-11-29 19:13:43 -0600

    Fixed scoring
    The score decreases/increases when good/bad food is removed from the plate.

--- Serious Nutrition Game/Assets/Stage 4/Scripts/Level3/Level3Scoring.cs
+++ Serious Nutrition Game/Assets/Stage 4/Scripts/Level3/Level3Scoring.cs
@@ -33,4 +33,22 @@ public class Level3Scoring : MonoBehaviour {
 			//gameObject.GetComponent<EdgeCollider2D> ().enabled = false;
 		}
 	}
+
+	void OnTriggerExit2D (Collider2D food) {
+
+		if (food.gameObject.CompareTag ("GoodFood"))
+		if (progressBar.value < 1f)
+			progressBar.value -= 0.2f;
+
+		if (food.gameObject.CompareTag ("BadFood"))
+		if (progressBar.value < 1f && progressBar.value > 0f)
+			progressBar.value += 0.05f;
+
+
+		if(progressBar.value >= 1f) {
+			Debug.Log ("You won the game!");
+			//gameObject.GetComponent<WinGame> ().Win ();
+			//gameObject.GetComponent<EdgeCollider2D> ().enabled = false;
+		}
+	}
 }
