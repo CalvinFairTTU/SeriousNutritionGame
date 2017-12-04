609adb29b611365f751deb373527d33dd2f45af8
Shashidhar Sastry <shashidsastry@gmail.com>
2017-12-03 20:28:24 -0600

    Finished Stage 4 Level 1

---
+++ Serious Nutrition Game/Assets/Stage 4/Scripts/Level1/PlayerControllerL1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControllerL1 : MonoBehaviour {
+
+	private Rigidbody2D rigidBody;
+	private float moveHorizontal = 0;
+	public Slider progressBar;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();	
+	}
+
+	void Update() {
+		if (transform.position.y > 3.6f)
+			transform.position = new Vector2 (transform.position.x, 3.6f);
+		if (transform.position.y < -3.6f)
+			transform.position = new Vector2(transform.position.x, -3.6f);
+	}
+
+	void FixedUpdate() {
+		float moveVertical = Input.GetAxis ("Vertical");
+		rigidBody.velocity = new Vector2 (moveHorizontal, moveVertical*10f);
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.CompareTag ("BadFood")) {
+			if (progressBar.value < 1f && progressBar.value > 0f)
+				progressBar.value -= 0.05f;
+			other.gameObject.SetActive (false);
+		}
+		else if (other.gameObject.CompareTag ("GoodFood")) {
+			other.gameObject.SetActive (false);
+			if (progressBar.value < 1f)
+				progressBar.value += 0.2f;
+		}
+
+		if (progressBar.value >= 1f) {
+			Debug.Log ("You won the game!");
+		}
+	}
+}
---
+++ Serious Nutrition Game/Assets/Stage 4/Scripts/Level1/spawnerL1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnerL1 : MonoBehaviour {
+
+	public GameObject[] foods;
+	public Vector3 [] whereToSpawn;
+	public float spawnRate = 2f;
+	float nextSpawn = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update () {
+		if (Time.time > nextSpawn) 
+		{
+			nextSpawn = Time.time + spawnRate;
+			GameObject spanObject = foods [Random.Range (0, foods.Length)];
+			Vector3 spawnPlace = whereToSpawn [Random.Range (0, whereToSpawn.Length)];
+			Instantiate (spanObject, spawnPlace, Quaternion.identity);
+		}
+	}
+	/*
+	// Update is called once per frame
+	void Update () {
+		countdown -= Time.deltaTime ();
+		if (countdown <= 0) 
+		{
+			SpawnFood ();
+			countdown = waitingForNextSpawn;
+		}
+	}
+
+	void SpawnFood () {
+		Vector2 pos = new Vector2(-7 , 0);
+		GameObject goodsPrefab = goodFoods [Random.Range(0, goodFoods.Length)];
+		Instantiate(goodsPrefab, pos);
+
+	}
+	*/
+
+}
